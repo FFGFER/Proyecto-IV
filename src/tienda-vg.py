e4cc4c8201eabd35da3938613fa25ab29032a2c1
FFGFER <ffgfer@gmail.com>
2018-11-05 08:19:25 +0100

    Arreglando nombre de fichero de la app, añadiendo Procfile closes #11

---
+++ src/tienda-vg.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, session, redirect, url_for, escape, request, jsonify
+from jinja2 import Markup
+import videojuegos
+
+app = Flask(__name__)
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/')
+def index():
+    return jsonify(status="Ok")
+
+@app.route('/index')
+def index2():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
